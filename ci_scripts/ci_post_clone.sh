349c564b3a695e480dd4fc4346b0ed5e35986176
Alan <mvpi3.tw@gmail.com>
2024-02-18 17:34:23 +0800

    update ci_scripts

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -7,6 +7,11 @@ echo 'export GEM_HOME=$HOME/gems' >>~/.bash_profile
 echo 'export PATH=$HOME/gems/bin:$PATH' >>~/.bash_profile
 export GEM_HOME=$HOME/gems
 export PATH="$GEM_HOME/bin:$PATH"
+
+
+echo ">>> BREW UPDATE RUBY"
+brew update
+brew install ruby
  
 echo ">>> INSTALL DEPENDENCIES"
 gem install cocoapods --install-dir $GEM_HOME
