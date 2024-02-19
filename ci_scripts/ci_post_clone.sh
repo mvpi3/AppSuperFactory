6f344bba156329f43cb5a773d4e6c5c55ba04417
Alan <mvpi3.tw@gmail.com>
2024-02-19 10:58:12 +0800

    just use brew install cocoapods

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
  
-cd ..
+#cd ..
  
-echo ">>> SETUP ENVIRONMENT"
-echo 'export GEM_HOME=$HOME/gems' >>~/.bash_profile
-echo 'export PATH=$HOME/gems/bin:$PATH' >>~/.bash_profile
-export GEM_HOME=$HOME/gems
-export PATH="$GEM_HOME/bin:$PATH"
+#echo ">>> SETUP ENVIRONMENT"
+#echo 'export GEM_HOME=$HOME/gems' >>~/.bash_profile
+#echo 'export PATH=$HOME/gems/bin:$PATH' >>~/.bash_profile
+#export GEM_HOME=$HOME/gems
+#export PATH="$GEM_HOME/bin:$PATH"
 
 
-echo ">>> BREW UPDATE RUBY"
-brew update
-brew install ruby
+#echo ">>> BREW UPDATE RUBY"
+#brew update
+#brew install ruby
+
+brew install cocoapods
  
-echo ">>> INSTALL DEPENDENCIES"
-gem install cocoapods --install-dir $GEM_HOME
+#echo ">>> INSTALL DEPENDENCIES"
+#gem install cocoapods --install-dir $GEM_HOME
  
 echo ">>> INSTALL PODS"
 pod install
