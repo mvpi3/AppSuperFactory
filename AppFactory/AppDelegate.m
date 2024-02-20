5f72de436c638aad6dbc1c8d3ae67d26202ebdf6
Alan <mvpi3.tw@gmail.com>
2024-02-20 15:39:50 +0800

    測試 uploading to app store

--- AppFactory/AppDelegate.m
+++ AppFactory/AppDelegate.m
@@ -27,6 +27,7 @@
 //    self.window.backgroundColor = [UIColor whiteColor];
 //    [self.window makeKeyAndVisible];
     
+    
     TabBarViewController *tab = [[TabBarViewController alloc] init];
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.rootViewController = tab;
