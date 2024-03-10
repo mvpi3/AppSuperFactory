f5269cfff355db02c230bcec5feed4ff4597e023
Alan <mvpi3.tw@gmail.com>
2024-03-10 16:02:09 +0800

    Add script

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,25 +1,7 @@
 #!/bin/sh
  
-#cd ..
- 
-#echo ">>> SETUP ENVIRONMENT"
-#echo 'export GEM_HOME=$HOME/gems' >>~/.bash_profile
-#echo 'export PATH=$HOME/gems/bin:$PATH' >>~/.bash_profile
-#export GEM_HOME=$HOME/gems
-#export PATH="$GEM_HOME/bin:$PATH"
-
-
-#echo ">>> BREW UPDATE RUBY"
-#brew update
-#brew install ruby
-
+echo ">>> INSTALL COCOAPODS"
 brew install cocoapods
-
-echo ">>> INSTALL OCLINT"
-brew install oclint
- 
-#echo ">>> INSTALL DEPENDENCIES"
-#gem install cocoapods --install-dir $GEM_HOME
  
 echo ">>> INSTALL PODS"
 pod install
